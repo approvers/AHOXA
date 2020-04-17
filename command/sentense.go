c23b2e0471c7857f25666f2fa016db902bcfc1fc
raiga0310 <raiga0310brilio@gmail.com>
2020-04-18 00:41:37 +0900

    文章変更

--- command/sentense.go
+++ command/sentense.go
@@ -1,6 +1,10 @@
 package command
 
 var (
+	advise = "usage: !status [emoji]"
+	forbidden = "権限がないので変更できません。落ちぶれましょう。"
 	helloWorld = "Hello, world!"
-	usageSentence = "試作段階のBotです。現在実装されているコマンドは、\n!hello: 挨拶をします。\n!name: 発言者の名前を表示します。\n!id: 発言者のIDを表示します。\n!unko: (WIP)発言者の表示名を:poop:にします。現在開発中なので正常に動作しません。"
+	notify = "名前を元に戻しました"
+	usageSentence = "試作段階のBotです。現在実装されているコマンドは、\n!hello: 挨拶をします。\n!name: 発言者の名前を表示します。\n!id: 発言者のIDを表示します。\n!status [Emoji]: 表示名を絵文字にします。\n!reset: 名前をユーザーネームに戻します。"
+	wrong = "あなたのコードが間違っています"
 )
