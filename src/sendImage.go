e2f20cdc84cc3cc9fa0e4585b6f800d944866c28
raiga0310 <raiga0310brilio@gmail.com>
2020-05-16 01:41:14 +0900

    [fix]修正(２)
    何をしているかわからなくなったので聖火を上げます

--- src/sendImage.go
+++ src/sendImage.go
@@ -12,10 +12,34 @@ import (
 	"strings"
 )
 
+func ParseColorCode(colorCode string) (Result color.RGBA, Err error) {
+	red, Err := strconv.ParseInt(colorCode[0:2],16,32)
+	if Err != nil {
+		log.Println(fmt.Sprintf("Red --- strconv.ParseInt: %s", Err))
+		return
+	}
+	green, Err := strconv.ParseInt(colorCode[2:4],16,32)
+	if Err != nil {
+		log.Println(fmt.Sprintf("Green --- strconv.ParseInt: %s", Err))
+		return
+	}
+	blue, Err := strconv.ParseInt(colorCode[4:6],16,32)
+	if Err != nil {
+		log.Println(fmt.Sprintf("Blue --- strconv.ParseInt: %s", Err))
+		return
+	}
+	log.Printf("\"%s\" parsed as %d, %d, %d,", colorCode, red, green, blue)
+	Result.R = byte(red)
+	Result.G = byte(green)
+	Result.B = byte(blue)
+	return
+
+}
+
 func GenerateImage(session *discordgo.Session, message *discordgo.MessageCreate) {
 	const (
-		x      = 0
-		y      = 0
+		statrX = 0
+		startY = 0
 		width  = 40
 		height = 30
 	)
@@ -31,7 +55,7 @@ func GenerateImage(session *discordgo.Session, message *discordgo.MessageCreate)
 
 	colorCode := content[len("#"):]
 	if len(colorCode) != 6 {
-		log.Println("generateImage: len(colorCode) != 6")
+		log.Println("generateImage: len(colorCode) must be just 6")
 		_, Err := session.ChannelMessageSend(message.ChannelID, "不正な値です。形式は16進のカラーコードである必要があります。")
 		if Err != nil {
 			log.Println(Err)
@@ -39,25 +63,14 @@ func GenerateImage(session *discordgo.Session, message *discordgo.MessageCreate)
 		return
 	}
 
-	img := image.NewRGBA(image.Rect(x, y, width, height))
 
-	red, Err := strconv.ParseInt(colorCode[0:2],16,32)
-	if Err != nil {
-		log.Println(fmt.Sprintf("strconv.ParseInt: %s",Err))
-	}
-	green, Err := strconv.ParseInt(colorCode[2:4],16,32)
-	if Err != nil {
-		log.Println(fmt.Sprintf("strconv.ParseInt: %s",Err))
-	}
-	blue, Err := strconv.ParseInt(colorCode[4:6],16,32)
-	if Err != nil {
-		log.Println(fmt.Sprintf("strconv.ParseInt: %s",Err))
-	}
 
-	log.Printf("\"%s\" parsed as %d, %d, %d,", colorCode, red, green, blue)
-	for i := img.Rect.Min.Y; i < img.Rect.Max.Y; i++ {
-		for j := img.Rect.Min.X; j < img.Rect.Max.X; j++ {
-			img.Set(j, i, color.RGBA{R: byte(red), G: byte(green), B: byte(blue)})
+	img := image.NewRGBA(image.Rect(statrX, startY, width, height))
+
+	colorInfo, Err := ParseColorCode(colorCode)
+	for x := img.Rect.Min.Y; x < img.Rect.Max.Y; x++ {
+		for y := img.Rect.Min.X; y < img.Rect.Max.X; y++ {
+			img.Set(x, y, colorInfo)
 		}
 	}
 
