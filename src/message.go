6f16abdec68e9bf841d87c69884ea9b3e582c498
raiga0310 <raiga0310brilio@gmail.com>
2020-05-18 12:20:16 +0900

    [fix]定型文の編集

--- src/message.go
+++ src/message.go
@@ -2,7 +2,7 @@ package src
 
 var (
 	helpMessage = "```asciidoc\n" +
-`= Title =
+		`= Title =
 	something
 = Description =
 	表示名に絵文字を付けることでステータスを表示するBotの予定
@@ -10,8 +10,9 @@ var (
 	(Prefix: "%")
 	help :: このBotの概要を説明します。
 	ping :: Pong!と返します。かわいいですね。
-	(prefix: "#")
-	[HexRGB] :: 16進カラーコードの画像を返します。略記法は現在対応していません。
+	color :: 第二引数に以下の形式で入力:
+		(prefix: "#")
+		[HexRGB] :: 16進カラーコードの画像を返します。略記法は現在対応していません。
 = Source =
 	github.com/brokenManager/change-status-go
 ` + "```\n"
